576cc3d5d5ac1a4de7a8dfd52498c9c284fa13bf
la <502736806@qq.com>
2019-08-11 18:02:58 +0800

    更新

--- rider.py
+++ rider.py
@@ -6,4 +6,17 @@ class Rider(pygame.sprite.Sprite):
         
         self.image = pygame.image.load('C:/Users/lalala/Documents/GitHub/BUPT1-pygame/素材/骑手红.jpg').convert_alpha()
         self.rect = self.image.get_rect()
+        self.speed = 10
+    # 向上运动
+    def move_up(self):
+        self.rect.top -= self.speed
+    # 向下运动
+    def move_down(self):
+        self.rect.top += self.speed
+    # 向左运动
+    def move_left(self):
+        self.rect.left -= self.speed
+    # 向右运动
+    def move_right(self):
+        self.rect.right += self.speed
         
