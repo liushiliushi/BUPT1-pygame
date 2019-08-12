daa152550c76f5b7764ad5b312734586ae15f6ad
la <502736806@qq.com>
2019-08-12 16:51:33 +0800

    修改

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pygame
 import sys
 
-bg_size = width, height = 900, 600 #屏幕大小尺寸
+bg_size = width, height = 800, 800 #屏幕大小尺寸
 screen = pygame.display.set_mode(bg_size) 
-rider = pygame.image.load('C:/Users/lalala/Documents/GitHub/BUPT1-pygame/素材/骑手红.jpg')
-pygame.display.set_caption('外卖订单派送系统').convert()
+rider1 = pygame.image.load('C:/Users/lalala/Documents/GitHub/BUPT1-pygame/素材/骑手小.png').convert_alpha()
+house1 = pygame.image.load('C:/Users/lalala/Documents/GitHub/BUPT1-pygame/素材/食客.png').convert_alpha()
+house = pygame.transform.scale(house1, (100, 100))
+rider = pygame.transform.scale(rider1, (30, 30))
+pygame.display.set_caption('外卖订单派送系统')
 
 def main():
     # 初始化
@@ -16,6 +19,11 @@ def main():
                 pygame.quit()
                 sys.exit()
         screen.fill((255, 255, 204))
+        # 绘制房子
+        for i in range(0, 900, 100):
+            for j in range(0, 900, 100):
+                screen.blit(house, (i, j))
+        screen.blit(rider,(385, 385))
         pygame.display.update() 
         clock.tick(60) # 设置帧数
 
